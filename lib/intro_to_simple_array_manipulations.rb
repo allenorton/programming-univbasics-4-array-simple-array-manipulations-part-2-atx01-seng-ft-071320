66c6b622b18f64067cd106e57c291df89b06c752
Allene Norton <norton.allene@gmail.com>
2020-06-10 20:40:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,10 +12,11 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  
+  array.flatten 
 end
 
-def using_delete
+def using_delete(array, string)
+  array.delete(string)
 end
 
 def using_delete_at
